6d9d846573763812c2fa6e8b7262b28f47aeced0
umerovzafar <umerov.99@gmail.com>
2023-02-10 16:39:20 +0500

    movieDB

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:movie_db/domain/hive/favourite_list.dart';
+import 'package:movie_db/domain/hive/hive_boxes.dart';
+import 'package:movie_db/movie_app.dart';
+
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+  await Hive.initFlutter();
+  Hive.registerAdapter(FavouriteListAdapter());
+
+  await Hive.openBox<FavouriteList>(HiveBoxes.favouriteBox);
+  await dotenv.load(fileName: '.env');
+
+  runApp(
+    const MovieApp(),
+  );
+}
---
+++ lib/movie_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:movie_db/domain/provider/movie_provder.dart';
+import 'package:movie_db/ui/routes/app_navigator.dart';
+import 'package:provider/provider.dart';
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MovieProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: AppNavigator.initRoute,
+        routes: AppNavigator.routes,
+        onGenerateRoute: AppNavigator.generate,
+      ),
+    );
+  }
+}
